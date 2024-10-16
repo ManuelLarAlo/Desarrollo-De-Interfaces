39e99607bb8787bc23117ad2ea6888ac180c332f
ManuelLarAlo <manuellaraalos@gmail.com>
2024-10-16 10:08:04 +0200

    Limpieza del repositorio

--- country_app/android/app/src/main/kotlin/com/example/country_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.country_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- practica1flutter/android/app/src/main/kotlin/com/example/practica1flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.practica1flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
